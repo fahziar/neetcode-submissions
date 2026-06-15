3a93f5b1cf109afe35a6fa743e163e559abd2f6c
fahziar <fahziar@users.noreply.github.com>
2026-06-15 21:09:00 +0700

    Add: generate-parentheses - submission-1

---
+++ Data Structures & Algorithms/generate-parentheses/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        result = []
+
+        def recursive(curr, remaining, openCount, closeCount) :
+            if remaining == 0 :
+                currResult = curr
+                while closeCount != openCount:
+                    currResult = currResult + ")"
+                    closeCount = closeCount + 1
+                result.append(currResult)
+                return
+            
+            # option 1 : decide to open new parentheses
+            recursive(curr + "(", remaining - 1, openCount + 1, closeCount)
+
+            # option 2 : decide to close parentheses
+            if closeCount < openCount :
+                recursive(curr + ")", remaining, openCount, closeCount + 1)
+        
+        recursive("(", n - 1, 1, 0)
+        return result
