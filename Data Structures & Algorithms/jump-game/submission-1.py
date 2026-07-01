993d8d4e392319d58c3fe8491daaccbd028aa614
fahziar <fahziar@users.noreply.github.com>
2026-07-01 10:43:23 +0700

    Add: jump-game - submission-1

---
+++ Data Structures & Algorithms/jump-game/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        reachable = [False for i in nums]
+        for i in range(len(nums) - 1, -1, -1) :
+            if i + nums[i] + 1 >= len(nums) :
+                reachable[i] = True
+            else :
+                for j in range(i, i + nums[i] + 1) :
+                    if reachable[j]:
+                        reachable[i] = True
+                        break
+        
+        return reachable[0]
+        
