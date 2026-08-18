924928d84c6531a2f1d3b06c9f05c1e30f504f3e
fahziar <fahziar@users.noreply.github.com>
2026-08-18 09:01:57 +0700

    Add: combination-target-sum - submission-2

---
+++ Data Structures & Algorithms/combination-target-sum/submission-2.py
@@ -0,0 +1,26 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        result = []
+
+        def recursive(i, currNums, currSum) :
+            if i == len(nums) :
+                return
+            
+            if currSum + nums[i] == target :
+                currResult = currNums[:]
+                currResult.append(nums[i])
+                result.append(currResult)
+                # backtrack
+                return
+            if currSum + nums[i] < target :
+                currSum = currSum + nums[i]
+                currNums.append(nums[i])
+                for i in range(i, len(nums)) :
+                    recursive(i, currNums, currSum)
+                currNums.pop()
+            
+
+        for i in range(0, len(nums)) :
+            recursive(i, [], 0)
+        return result
+        
